1f3b63355f0e33a964aa5a11be81e0ea071a7d3b
Joshua Eichorn <joshua.eichorn@pagely.com>
2014-08-05 22:47:50 +0000

    Add some code to cleanup stale watches

--- src/NginxCP/Inotify.php
+++ src/NginxCP/Inotify.php
@@ -4,11 +4,15 @@ namespace NginxCP;
 class Inotify 
 {
 	protected $proc;
+    protected $cmd = 'inotifywait -e delete -e moved_to -e close_write -e create -e delete -rm';
 
 	public function __construct($path)
 	{
+        // we can leak these guys so cleanup on start
+        $this->killStaleInotify();
+
 		// start a inotifywait process
-		$this->proc = popen("inotifywait -e delete -e moved_to -e close_write -e create -e delete -rm $path", "r");
+		$this->proc = popen("$this->cmd $path", "r");
         if (!$this->proc)
         {
             throw new \Exception("Runing inotifywait failed");
@@ -32,4 +36,17 @@ class Inotify
 
 		return $updates;
 	}
+
+    public function killStaleInotify()
+    {
+        exec('ps aux | grep '.escapeshellarg($this->cmd).' | grep -v grep', $output);
+        foreach($output as $line)
+        {
+            if (preg_match('/^[a-zA-Z-]+ ([0-9]+) /', $line, $match))
+            {
+                $pid = $match[1];
+                posix_kill($pid, SIGKILL);
+            }
+        }
+    }
 }
